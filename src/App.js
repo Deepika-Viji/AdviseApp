195034faf86a77bd371e8483c8f11787fdb77a71
deepika@riversilica.com <deepika@riversilica.com>
2025-01-06 21:40:51 +0530

    Your commit message

--- src/App.js
+++ src/App.js
@@ -22,17 +22,26 @@ const App = () => {
   },[]);
 
   return (
-    <div className='bg-slate-600 min-h-screen'>
-        <div className="flex items-center justify-center min-h-screen">
-          <div className="text-center">
-            <div className='text-center text-white font-extrabold text-3xl'>ID:{id}</div>
-            <div className="text-white font-bold text-2xl mb-4">ADVICE</div>
-            <div className="h-52 w-[500px] border border-white flex items-center justify-center">
-              <div className="text-white font-bold text-2xl">{advise}</div>
-            </div>
-          </div>
-        </div>
+    // <div className='bg-slate-600 min-h-screen'>
+    //     <div className="flex items-center justify-center min-h-screen">
+    //       <div className="text-center">
+    //         <div className='text-center text-white font-extrabold text-3xl'>ID:{id}</div>
+    //         <div className="text-white font-bold text-2xl mb-4">ADVICE</div>
+    //         <div className="h-52 w-[500px] border border-white flex items-center justify-center">
+    //           <div className="text-white font-bold text-2xl">{advise}</div>
+    //         </div>
+    //       </div>
+    //     </div>
+    // </div>
+    <div className="bg-slate-600 min-h-screen flex items-center justify-center">
+    <div className="text-center p-4">
+      <div className="text-white font-extrabold text-2xl sm:text-3xl mb-4">ID: {id}</div>
+      <div className="text-white font-bold text-xl sm:text-2xl mb-4">ADVICE</div>
+      <div className="h-auto w-full sm:w-[500px] max-w-full border border-white flex items-center justify-center p-4">
+        <div className="text-white font-bold text-lg sm:text-2xl">{advise}</div>
+      </div>
     </div>
+  </div>
   )
 }
 
